89aadde3a2e4c505a89a1d57fec92080d8a43064
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2025-07-04 19:09:17 -0400

    initial setup with tailwind and google fonts config

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-500 font-playful">this is the heading</h1>
+      <h6 className="text-green-400 font-lato">this the body</h6>
+      <h1>this is the default</h1>
+    </div>
+  );
+};
+
+export default App;
