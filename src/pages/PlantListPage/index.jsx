f9f6cd8ad6d340daea347d974620ab3f96c81c92
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2025-07-26 19:19:07 -0400

    get plants when the PlantsListPage mounts

--- src/pages/PlantListPage/index.jsx
+++ src/pages/PlantListPage/index.jsx
@@ -1,7 +1,17 @@
 import NavBar from "shared-components/NavBar";
 import RedirectToSignedInIfSignedOut from "shared-components/RedirectToPlantsIfSignedInIfSignedOut";
+import { useEffect } from "react";
+import * as plantService from "services/plant";
 
 const PlantListPage = () => {
+  useEffect(() => {
+    (async () => {
+      const response = await plantService.getPlants();
+      const data = await response.json();
+      console.log(data);
+    })();
+  }, []);
+
   return (
     <RedirectToSignedInIfSignedOut>
       <NavBar />
